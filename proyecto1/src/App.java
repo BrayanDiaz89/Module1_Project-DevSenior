186b30c06d31541520c33b9d10780c7d0ca4fe03
Brayan Díaz <brayandiaz258f@gmail.com>
2025-07-03 23:28:02 -0500

    Creación completa del registro de estudiante y método para mostrar datos del estudiante actual.

--- proyecto1/src/App.java
+++ proyecto1/src/App.java
@@ -31,7 +31,7 @@ public class App {
                     registrarEstudiante(keyboard);
                     break;
                 case 2:
-                    
+                    System.out.println(mostrarDatosEstudianteActual()); 
                     break;
                 case 3:
                     
@@ -56,11 +56,38 @@ public class App {
                 nombreEstudiante = keyboard.nextLine().strip();
                 nombreEsValido = validarNombre(nombreEstudiante);
             }
+
+            notaOne = validarNota(keyboard, 1);
+            notaTwo = validarNota(keyboard, 2);
+            notaThree = validarNota(keyboard, 3);
             
+            System.out.printf("El registro del estudiante %s fue exitoso.\nSus datos son: \n", nombreEstudiante);
+            String estudiante = mostrarDatosEstudianteActual();
+            System.out.println(estudiante);
         }
 
         private static boolean validarNombre(String nombre){
             return !(nombre.isBlank()) ? true : false;
         }
+    
+        private static double validarNota(Scanner keyboard, int consecutivo){
+            System.out.printf("Digite la nota %d: ", consecutivo);
+            double nota = keyboard.nextDouble();
+            while (!(nota > 0 && nota <= 100)) {
+                System.out.printf("La nota %d, no es válida, ingresela nuevamente: ", consecutivo);
+                nota = keyboard.nextDouble();
+            }
+            return nota;
+        }
+
+        private static String mostrarDatosEstudianteActual(){
+            return String.format("""
+                Estudiante:  %s
+                Nota 1: %.2f
+                Nota 2: %.2f
+                Nota 3: %.2f
+                """, nombreEstudiante, notaOne, notaTwo, notaThree);
+        } 
+
     }
 
